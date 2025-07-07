d27e588d733a161927b7c92fb5476b4f754a766a
mtcodingtech <mtcodingtech@gmail.com>
2025-07-07 22:10:14 +0630

    day4 js

---
+++ JS/Day4/DOM.js
@@ -0,0 +1,9 @@
+// document.getElementById("para1").innerHTML = "Hello";
+
+// let demo = document.getElementsByClassName("demo");
+// demo[0].innerHTML = "Hello World";
+// demo[0].style.color = "red";
+// demo[0].style.backgroundColor = "pink";
+
+let para = document.querySelector("p");
+console.log(para)
---
+++ JS/Day4/string-methods.js
@@ -0,0 +1,8 @@
+// let fruit = "this apple";
+// console.log(fruit.length)
+
+// ****************
+
+let fruit = "applep";
+// console.log(fruit.indexOf("p"))
+console.log(fruit.lastIndexOf("p"))
